036524899679d4174a4055ac7d39662c9cb207b7
hammad <muhammad-Hammad-19@gmail.com>
2025-10-24 13:59:38 +0500

    react issue resolved

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router";
 import { collection, onSnapshot } from "firebase/firestore";
 import jsPDF from "jspdf";
 
+
 export default function Dashboard() {
   const [user, setUser] = useState(null);
   const [pitches, setPitches] = useState([]);
